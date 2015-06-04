7d233c5e664e2c9b81e3325e7ddcd04a8bdae090
Göran Törnqvist <goran@tornqvist.ws>
2015-06-04 12:16:22 +0200

    v. 0.2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /* 
-Dreambox Transcoding Manager - v. 0.1
+Dreambox Transcoding Manager - v. 0.2
 
 The MIT License (MIT)
 
@@ -26,6 +26,7 @@ SOFTWARE.
 */
 session_start();
 $config = parse_ini_file("config.php", true);
+$ctx = stream_context_create(array('http' => array('timeout' => 3)));
 
 function logger($logfile, $logstring) {
   $fh = fopen($logfile, 'a');
@@ -33,41 +34,38 @@ function logger($logfile, $logstring) {
   fclose($fh);
 }
 
-function ip_on_network($mask, $ip) {
-    @list($net, $bits) = explode('/', $mask);
-    $bits = isset($bits) ? $bits : 32;
-    $bitmask = -pow(2, 32-$bits) & 0x00000000FFFFFFFF;
-    $netmask = ip2long($net) & $bitmask;
-    $ip_bits = ip2long($ip)  & $bitmask;
-    return (($netmask ^ $ip_bits) == 0);
-}
-
 function dreamboxChangeChannel($version, $ip, $channel) {
-  if ($version == "1") {
-    $result = file_get_contents("http://$ip/cgi-bin/zapTo?path=" . urlencode($channel));
-    return true;
-  } else {
-    $result = file_get_contents("http://$ip/web/zap?sRef=" . urlencode($channel));
-    if (preg_match("/\<e2state\>True\<\/e2state\>/", $result)) {
+  $ctx = stream_context_create(array('http' => array('timeout' => 3)));
+  if ($channel != '') {
+    if ($version == "1") {
+      $result = @file_get_contents("http://$ip/cgi-bin/zapTo?path=" . urlencode($channel), 0, $ctx);
       return true;
     } else {
-      return false;
+      $result = @file_get_contents("http://$ip/web/zap?sRef=" . urlencode($channel), 0, $ctx);
+      if (preg_match("/\<e2state\>True\<\/e2state\>/", $result)) {
+        return true;
+      } else {
+        return false;
+      }
     }
+  } else {
+    return false;
   }
 }
 
 function dreamboxGetChannel($version, $ip) {
+  $ctx = stream_context_create(array('http' => array('timeout' => 3)));
   if ($version == "1") {
     $url = "http://$ip/channels/getcurrent";
-    $result = file_get_contents($url);
+    $result = @file_get_contents($url, 0, $ctx);
     if ($result != FALSE) {
       return $result;
     } else {
-      return "Could not get current channel from $url";
+      return "N/A";
     }
   } else {
     $url = "http://$ip/web/getcurrent";
-    $result = file_get_contents($url);
+    $result = @file_get_contents($url, 0, $ctx);
     if ($result != FALSE) {
       $doc = new DOMDocument();
       libxml_use_internal_errors(true);
@@ -78,10 +76,10 @@ function dreamboxGetChannel($version, $ip) {
       if (strlen($channel) > 1) {
         return $channel;
       } else {
-        return "Could not parse channel xml from $url";
+        return "N/A";
       }
     } else {
-      return "Could not get current channel from $url";
+      return "N/A";
     }
   }
 }
@@ -89,24 +87,7 @@ function dreamboxGetChannel($version, $ip) {
 $title = $config['global']['title'];
 $webaddress = $config['global']['webaddress'];
 
-# We check the main iptables config file to verify when new clients are written to it by the cron job script iptables-vlc-clients.sh
-$iptablesfile="/etc/sysconfig/iptables";
-
-# This is our "database" of allowed clients where we read and add new IP addresses
-$vlcclientsfile=$config['global']['clientsfile'];
-
 $applog=$config['global']['datadir'] . "/" . $config['global']['logfilename'];
-
-# Create an array of all IP addresses
-$filecontents = trim(file_get_contents($vlcclientsfile));
-$clients=split(",", $filecontents);
-
-# The IP address we use for adding to the vlcclientsfile
-if ($config['global']['reverse_proxy_mode'] == 'true') {
-	$remotehost = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-	$remotehost = $_SERVER['REMOTE_ADDR'];
-}
 
 # Get/set username session variable
 if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
@@ -121,33 +102,33 @@ if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
 $username = $_SESSION['username'];
 
 # Actions below
-switch ($_SERVER['QUERY_STRING']) {
-    case "addme":     
-      # Add remotehost to vlcclientsfile, but only if its not already there
-      if (array_search($remotehost,$clients) == FALSE) {
-        array_push($clients, $remotehost);
-        $newfilecontents=implode(",", $clients);
-        if (file_put_contents ($vlcclientsfile , $newfilecontents) != FALSE) {
-          $now=date('Y-m-d H:i:s');
-          logger($applog, "$now - User $username added $remotehost to vlc clients file\n");
-          $_SESSION['alertmessage'] = "Access for $remotehost is allowed, please wait a minute before firewall rules are reloaded ...";
-          header("Location: /");
-          exit;
-        } else {
-          $now=date('Y-m-d H:i:s');
-          logger($applog, "$now - User $username : Error writing vlc clients file\n");
-          $_SESSION['alertmessage'] = "Error writing vlc clients file!";
-          header("Location: /");
-          exit;
-        }
-      } else {
-        // remotehost already in vlcclientsfile for some reason.
-        header("Location: /");
+switch ((isset($_GET['action']) ? $_GET['action'] : '')) {
+  case "stream":
+    if ($_GET['box'] != '') {
+      $box = $_GET['box'];
+      if (array_key_exists($box, $config)) {
+        $ipaddress=$config[$box]['ipaddress'];
+        $port=$config[$box]['port'];
+        header("Content-Type: application/octet-stream");
+        $fd = fopen("http://localhost:$port/",'r');
+        fpassthru($fd);
         exit;
       }
-      break;
+    }
+  case "get-m3u-file":
+    if ($_GET['box'] != '') {
+      $box = $_GET['box'];
+      if (array_key_exists($box, $config)) {
+        $ipaddress=$config[$box]['ipaddress'];
+        $port=$config[$box]['port'];
+        header("Content-Type: audio/mpegurl");
+        header("Content-Disposition: attachment; filename=$box.m3u");
+        print "http://$webaddress/?action=stream&box=$box\r\n";
+        exit;
+      }
+    }
 
-    case "startserver":
+  case "startserver":
       # Start vlc transcoding process
       $channel = $_POST['channel'];
       $quality = $_POST['quality'];
@@ -160,7 +141,7 @@ switch ($_SERVER['QUERY_STRING']) {
       $logfile = $config['global']['datadir'] . "/" . $config[$dreambox]['ipaddress'] . ".log";
       $pidfile = $config['global']['datadir'] . "/" . $config[$dreambox]['ipaddress'] . ".pid";
       $port = $config[$dreambox]['port'];
-      $pid = file_get_contents($pidfile);
+      $pid = @file_get_contents($pidfile, 0, $ctx);
       if (is_numeric($pid) && intval($pid) > 0) {
         if (file_exists("/proc/$pid")) {
           $ret = exec("kill -9 $pid");
@@ -209,45 +190,18 @@ body {
   font-family: Verdana, Arial, "Times New Roman";
 }
 </style>
+<script language="javascript">
+
+</script>
 </head>
 <body>
 <?php
 echo "<h2>$title</h2>";
-echo "<table border=1><tr><td valign=top>";
-echo "<table border=0><tr><td><u>Currently allowed VLC Clients:</u></td></tr>";
-echo "<tr><td><div style='height:200px;width:250px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;'>";
-echo "<table border=0>";
-$is_allowed=false;
-$iptablesmatch = "";
-for ($i=0; $i<sizeof($clients); $i++) {
-  $client=$clients[$i];
-  if (strpos($client, "/") > 0) {
-    # Network
-    if (ip_on_network($client, $remotehost)) {
-      $is_allowed=true;
-      echo "<tr><td><b>" . $client  . "</b> (your subnet)</td></tr>";
-      $iptablesmatch = $client;
-    } else {
-      echo "<tr><td>" . $client  . "</td></tr>";
-    }
-  } else {
-    # Normal IP
-    if ($client == $remotehost) {
-      $is_allowed=true;
-      echo "<tr><td><b>" . $client  . "</b> (your IP)</td></tr>";
-      $iptablesmatch = $client;
-    } else {
-      echo "<tr><td>" . $client  . "</td></tr>";  
-    }
-  }
-}
-echo "</table></div></td></tr></table>";
-echo "</td><td valign=top>";
 echo "<table border=0>";
 echo "<tr><td><u>Logfile:</u></td></tr>";
 $file = file($applog);
 $file = array_reverse($file);
-echo "<tr><td><div style='height:200px;width:550px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;'>";
+echo "<tr><td><div style='height:200px;width:830px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;'>";
 $rows=0;
 foreach($file as $row){
     echo $row."<br />";
@@ -255,17 +209,7 @@ foreach($file as $row){
     if ($rows >= 100) break;
 }
 echo "</div></td></tr></table>";
-echo "</td></tr></table>";
-if ($is_allowed != true) {
-  echo "<br/><font color=red>Your IP address $remotehost is not in the allowed VLC clients list!</font><br/>";
-  echo "<a href=\"./?addme\">Click here to add it now</a>.<br/><br/>";
-} else {
-  $file = file_get_contents($iptablesfile);
-  $iptablesregex = str_replace("/", "\/", $iptablesmatch);
-  if (preg_match("/$iptablesregex,/", $file)) {
-
-    echo "<table border=0 width=814><td><td height=200><font color=green>Your IP is allowed in the firewall configuration. You can now open the URLs below in VLC player program by doing a copy/paste of the url field into VLC...</font><br/>Note:<br/> If the Transcode Stream Status is listed as down, you need to start it first by choosing channel/quality and clicking on 'Transcode Channel'. <br/>If the Transcode Stream Status is displayed as UP but you still cant connect, try restarting by click 'Transcode Channel' again since the transcoding may be stuck.<br/>And if that still doesnt help, it may be the outgoing firewall rules from where you are connecting from that are blocking your connections.</td></tr></table>";
-echo "
+echo "<br/><br/>
 <table border=1>
   <tr>
 ";
@@ -298,7 +242,7 @@ echo "
         }
 
 	$pidfile = $config['global']['datadir'] . "/" . $boxip . ".pid";
-	$pid = file_get_contents($pidfile);
+	$pid = @file_get_contents($pidfile, 0, $ctx);
 
         // Check if local vlc streaming port is up
         $clients="";
@@ -311,15 +255,13 @@ echo "
           $streamstatus = "<font color=green>Local port $streamport is UP</font>";
           // Create a thumbnail image of the stream using vlc
           exec($config['global']['vlc'] . " http://localhost:$streamport -I dummy --rate=1 --video-filter=scene --vout=dummy --start-time=1 --stop-time=1 --scene-format=jpg --scene-ratio=24 --scene-prefix=preview-$streamport- --scene-path=$previewdir --run-time=1 vlc://quit 1>/dev/null 2>&1");
+          $preview = true;
           // Check how many connections the stream currently has using netstat
           $cmd = "netstat -anp | grep ESTABLISHED | grep '$pid/vlc' | grep -v '$boxip' | awk '{ print $5 }' | cut -d':' -f1";
           $output = exec($cmd, $clients);
         } else {
+          $preview = false;
           $streamstatus = "<font color=red>Local port $streamport is DOWN</font>";
-          // Copy the N/A image to previewdir
-          if (!copy($previewdir . "na.jpg", $previewdir . "preview-" . $dreambox['port'] . "-00001.jpg")) {
-             $_SESSION['alertmessage'] .= " There was a problem setting preview image for port $streamport to na.jpg";
-          }
         }
 
         echo "
@@ -327,10 +269,6 @@ echo "
       <table border=0 width=407>
         <tr>
           <td colspan=2 align=center><b>" . $dreambox['description'] . "</b></td>
-        </tr>
-        <tr>
-          <td>url:</td>
-          <td>http://$webaddress:" . $dreambox['port'] . "</td>
         </tr>
         <tr>
           <td>Current Channel:</td>
@@ -355,7 +293,7 @@ echo "
           echo "<ul>";
           foreach ($clients as $client) {
             if ($client != '127.0.0.1')
-              echo "<li><a href='http://whois.domaintools.com/$client' target='_blank'>$client</a></li>";
+              echo "<li>$client</li>";
           }
           echo "</ul>";
         }
@@ -375,41 +313,44 @@ echo "
       if ($showtranscode) {
         echo "<tr>
           <td colspan=2>
-            <form method=\"POST\" action=\"./?startserver\">
+            <form method=\"POST\" action=\"./?action=startserver\">
               <input type=\"hidden\" name=\"dreambox\" value=\"$setting\"/>
               Channel/Quality:
-              <select name=\"channel\">";
+              <select name=\"channel\">
+                <option value=\"\"></option>";
       if ($dreambox['enigmaversion'] == "1") {
-        $html = file_get_contents("http://" . $dreambox['ipaddress'] . "/cgi-bin/getServices?ref=0");
-        $rows = str_getcsv($html, "\n"); 
-        foreach ($rows as &$row) {
-          // Hope User - bouquets (TV) works on all dreamboxes ;)
-          if (preg_match("/User - bouquets \(TV\)/", $row)) {
-            $row = str_getcsv($row, ";");
-            $ref = $row[0];
-            break;
+        $html = @file_get_contents("http://" . $dreambox['ipaddress'] . "/cgi-bin/getServices?ref=0", 0, $ctx);
+        if ($html != FALSE) {
+          $rows = str_getcsv($html, "\n"); 
+          foreach ($rows as &$row) {
+            // Hope User - bouquets (TV) works on all dreamboxes ;)
+            if (preg_match("/User - bouquets \(TV\)/", $row)) {
+              $row = str_getcsv($row, ";");
+              $ref = $row[0];
+              break;
+            }
           }
-        }
-        $html = file_get_contents("http://" . $dreambox['ipaddress'] . "/cgi-bin/getServices?ref=$ref");
-        $rows = str_getcsv($html, "\n");
-        foreach ($rows as &$row) {
-          if (preg_match("/" . preg_quote($dreambox['bouquet']) . "/", $row)) {
-            $row = str_getcsv($row, ";");
-            $ref = $row[0];
-            break;
+          $html = @file_get_contents("http://" . $dreambox['ipaddress'] . "/cgi-bin/getServices?ref=$ref", 0, $ctx);
+          $rows = str_getcsv($html, "\n");
+          foreach ($rows as &$row) {
+            if (preg_match("/" . preg_quote($dreambox['bouquet']) . "/", $row)) {
+              $row = str_getcsv($row, ";");
+              $ref = $row[0];
+              break;
+            }
           }
-        }
-        $html = file_get_contents("http://" . $dreambox['ipaddress'] . "/cgi-bin/getServices?ref=$ref");
-        $rows = str_getcsv($html, "\n");
-        foreach ($rows as &$row) {
-            $row = str_getcsv($row, ";");
-            $ref = $row[0];
-            $channel = $row[1];
-            echo "<option value=\"$ref\">$channel</option>";
+          $html = @file_get_contents("http://" . $dreambox['ipaddress'] . "/cgi-bin/getServices?ref=$ref", 0, $ctx);
+          $rows = str_getcsv($html, "\n");
+          foreach ($rows as &$row) {
+              $row = str_getcsv($row, ";");
+              $ref = $row[0];
+              $channel = $row[1];
+              echo "<option value=\"$ref\">$channel</option>";
+          }
         }
       } else {
         // Assume Enigma 2
-	$xml = file_get_contents("http://" . $dreambox['ipaddress'] . "/web/getservices");
+	$xml = @file_get_contents("http://" . $dreambox['ipaddress'] . "/web/getservices", 0, $ctx);
     	if ($xml != FALSE) {
 	        $doc = new DOMDocument();
 	        $doc->loadHTML(mb_convert_encoding($xml, 'HTML-ENTITIES', 'UTF-8'));
@@ -418,7 +359,7 @@ echo "
 	}
 	if (strlen($favouritesRef) > 10) {
 		# Get current channel and EPG from dreambox 1 - assumes no password on Enigma interface!
-	        $xml = file_get_contents("http://" . $dreambox['ipaddress'] . "/web/getservices?sRef=" . urlencode($favouritesRef));
+	        $xml = @file_get_contents("http://" . $dreambox['ipaddress'] . "/web/getservices?sRef=" . urlencode($favouritesRef), 0, $ctx);
         	if ($xml != FALSE) {
                 	$doc = new DOMDocument();
 	                $doc->loadHTML(mb_convert_encoding($xml, 'HTML-ENTITIES', 'UTF-8'));
@@ -443,16 +384,23 @@ echo "
         </tr>";
        } else {
          echo "<tr>
-          <td colspan=2 align=center height=50>You don´t have permission to operate this dreambox! You may however stream from it using VLC ...</td>
+          <td colspan=2 align=center height=50>You donÂ´t have permission to operate this dreambox! You may however stream from it using VLC ...</td>
         </tr>";
        }
         echo "
+        <tr>
+          <td colspan=2 align=center height=50><a href=\"./?action=get-m3u-file&box=$setting\">OPEN STREAM</a></td>
+        </tr>";
+       if ($preview) {
+         echo "
         <tr>
           <td colspan=2 align=center height=50>Preview</td>
         </tr>
         <tr>
           <td colspan=2 align=center><a href=\"preview/preview-" . $dreambox['port'] . "-00001.jpg\"><img src=\"preview/preview-" . $dreambox['port'] . "-00001.jpg\" width=\"300\"/></a><br/><br/></td>
-        </tr>
+        </tr>";
+       }
+       echo "
       </table>
     </td>
 ";
@@ -461,21 +409,7 @@ echo "
 echo "
   </tr>
 </table>
-<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 ";
-  }
-  else {
-    # IP exists in vlcclientsfile but hasnt been written to iptables config yet
-    echo "<table border=0 width=814><td><td height=100><font color=red>Firewall configuration has not been applied yet, please wait a minute and then refresh this page! (Auto refresh in 60 seconds)</font></td></tr></table>";
-    echo "
-<script type=\"text/javascript\">
-setTimeout(function(){
-   window.location.reload(1);
-}, 60000);
-</script>
-";
-  }
-}
 
 if(isset($_SESSION['alertmessage']) && !empty($_SESSION['alertmessage'])) {
 echo "
